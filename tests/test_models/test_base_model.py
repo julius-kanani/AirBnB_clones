9c93a318b081d12a3345dcabc54f8b7502703631
Julius M. Kanani <juliusm.kanani@gmail.com>
2024-02-09 00:11:29 +0300

    5 unittest tes cases for task 3.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ test_base_model module that supplies all unittest cases for the base module.
 """
 import unittest
 from models.base_model import BaseModel
+import uuid
 
 
 class TestBaseModel(unittest.TestCase):
@@ -25,3 +26,55 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.base_model, 'id'))
         self.assertTrue(hasattr(self.base_model, 'created_at'))
         self.assertTrue(hasattr(self.base_model, 'updated_at'))
+
+    def test_id_generation(self):
+        """
+        Unit test case if the id attribute is a valid UUID.
+        """
+
+        self.assertIsInstance(self.base_model.id, str)
+
+        # A valid UUID has 36 characters, including hyphens.
+        self.assertEqual(len(self.base_model.id), 36)
+
+        # Check if Id is in UUID format.
+        try:
+            uuid.UUID(self.base_model.id, version=4)
+        except ValueError:
+            self.fail("id is not a valid UUId.")
+
+    def test_str_method(self):
+        """
+        Test if the __str__ method returns the expected string representation.
+        """
+        expected_str = "[BaseModel] ({}) {}".format(
+            self.base_model.id, self.base_model.__dict__)
+
+        self.assertEqual(str(self.base_model), expected_str)
+
+    def test_save_method_updates_updated_at(self):
+        """
+        Test if the save method updates the updated_at attribute.
+        """
+        initial_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(initial_updated_at, self.base_model.updated_at)
+
+    def test_to_dict_method(self):
+        """
+        Test if the to_dict method returns the expected dictionary
+        representation.
+        """
+        obj = self.base_model
+
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+
+        # Test if to_dict method includes all instance attributes
+        for attr, value in obj.__dict__.items():
+            if attr not in ['created_at', 'updated_at']:
+                self.assertIn(attr, obj_dict)
+                self.assertEqual(obj_dict[attr], value)
